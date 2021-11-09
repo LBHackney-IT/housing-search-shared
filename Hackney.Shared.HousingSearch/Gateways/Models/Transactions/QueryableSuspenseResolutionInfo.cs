464e0c150db6050f546ccf4b9d4d128e7db0b9db
Anna <golosowa.anna@gmail.com>
2021-11-09 14:41:33 +0300

    Add models for Search Transactions endpoint (#7)

---
+++ Hackney.Shared.HousingSearch/Gateways/Models/Transactions/QueryableSuspenseResolutionInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Hackney.Shared.HousingSearch.Domain.Transactions;
+
+namespace Hackney.Shared.HousingSearch.Gateways.Models.Transactions
+{
+    public class QueryableSuspenseResolutionInfo
+    {
+        public SuspenseResolutionInfo Create()
+        {
+            return new SuspenseResolutionInfo
+            {
+                Note = Note,
+                IsApproved = IsApproved,
+                IsConfirmed = IsConfirmed,
+                ResolutionDate = ResolutionDate,
+            };
+        }
+
+        public DateTime? ResolutionDate { get; set; }
+
+        public bool IsConfirmed { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public string Note { get; set; }
+    }
+}
