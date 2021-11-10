1cddbc2cd1072de6efeea299e623d6aca6820bb7
golosowa.anna <golosowa.anna@gmail.com>
2021-11-10 14:35:24 +0300

    One more try to fix formatting

--- Hackney.Shared.HousingSearch/Gateways/Models/Transactions/QueryableTransaction.cs
+++ Hackney.Shared.HousingSearch/Gateways/Models/Transactions/QueryableTransaction.cs
@@ -11,8 +11,8 @@ namespace Hackney.Shared.HousingSearch.Gateways.Models.Transactions
         public Transaction ToTransaction()
         {
             return Transaction.Create(TargetId, TargetType, PeriodNo, FinancialYear, FinancialMonth,
-                TransactionSource, TransactionType, TransactionDate, TransactionAmount, PaymentReference, 
-                BankAccountNumber, IsSuspense, SuspenseResolutionInfo.ToSuspenseResolutionInfo(), PaidAmount, ChargedAmount, 
+                TransactionSource, TransactionType, TransactionDate, TransactionAmount, PaymentReference,
+                BankAccountNumber, IsSuspense, SuspenseResolutionInfo.ToSuspenseResolutionInfo(), PaidAmount, ChargedAmount,
                 BalanceAmount, HousingBenefitAmount, Address, Person.ToPerson(), Fund);
         }
 
