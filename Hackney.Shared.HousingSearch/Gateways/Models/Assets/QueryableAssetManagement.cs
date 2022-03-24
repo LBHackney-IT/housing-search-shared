96486d3f543e483f0dd6ae1d46d09d76bea2fb19
Sandip <Sandip@DESKTOP-8GSTDPG>
2022-03-24 18:40:40 +0530

    ManagingOrganisationId type changed to string

--- Hackney.Shared.HousingSearch/Gateways/Models/Assets/QueryableAssetManagement.cs
+++ Hackney.Shared.HousingSearch/Gateways/Models/Assets/QueryableAssetManagement.cs
@@ -8,7 +8,7 @@ namespace Hackney.Shared.HousingSearch.Gateways.Models.Assets
         public string AreaOfficeName { get; set; }
         public bool IsCouncilProperty { get; set; }
         public string ManagingOrganisation { get; set; }
-        public Guid ManagingOrganisationId { get; set; }
+        public string ManagingOrganisationId { get; set; }
         public string Owner { get; set; }
         public bool IsTMOManaged { get; set; }
         public string PropertyOccupiedStatus { get; set; }
