8cbabf191fadb09df0da870401507e2a5c028b26
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-09-07 11:19:10 +0100

    Add patchassignment to process

--- Hackney.Shared.HousingSearch/Domain/Process/Process.cs
+++ Hackney.Shared.HousingSearch/Domain/Process/Process.cs
@@ -11,36 +11,9 @@ namespace Hackney.Shared.HousingSearch.Domain.Process
         public TargetType TargetType { get; set; }
         public List<RelatedEntity> RelatedEntities { get; set; }
         public ProcessName ProcessName { get; set; }
+        public PatchAssignment PatchAssignment { get; set; }
         public ProcessState CurrentState { get; set; }
         public List<ProcessState> PreviousStates { get; set; }
         public int? VersionNumber { get; set; }
-
-        public Process() { }
-
-        public Process(Guid id, Guid targetId, TargetType targetType, List<RelatedEntity> relatedEntities, ProcessName processName, ProcessState currentState, List<ProcessState> previousStates, int? versionNumber)
-        {
-            Id = id;
-            TargetId = targetId;
-            TargetType = targetType;
-            RelatedEntities = relatedEntities;
-            ProcessName = processName;
-            CurrentState = currentState;
-            PreviousStates = previousStates;
-            VersionNumber = versionNumber;
-        }
-
-        public Task AddState(ProcessState updatedState)
-        {
-            if (CurrentState != null) PreviousStates.Add(CurrentState);
-            CurrentState = updatedState;
-
-            return Task.CompletedTask;
-        }
-
-        public static Process Create(Guid targetId, TargetType targetType, List<RelatedEntity> relatedEntities, ProcessName processName)
-        {
-
-            return new Process(Guid.NewGuid(), targetId, targetType, relatedEntities, processName, null, new List<ProcessState>(), null);
-        }
     }
 }
